4938bdb82dae022578162b598910e8e4f46bb12b
applepatel <63281449+applepatel@users.noreply.github.com>
2021-12-19 22:47:35 -0800

    Update index.php

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
 
 <!-- Home Page Section-->
 
-    <section class="Home" id="home">
+    <section class="Home" id="Home">
         <div class="video-container">
             <video src="img/v1.mp4" type="video/mp4" autoplay loop muted class="vid"></video>
         </div>
