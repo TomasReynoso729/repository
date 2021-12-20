ce4945ce692f068181b771cf66ab53f42c20beae
TomasReynoso729 <89235288+TomasReynoso729@users.noreply.github.com>
2021-12-19 17:54:37 -0800

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Foods</title>
+    <title>Recipe Resort</title>
     <link rel="stylesheet" href="css/style2.css">
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;600&display=swap" rel="stylesheet">">
@@ -25,17 +25,7 @@
         <div id="menu" class="fas fa-bars"></div>
 
         <nav class="navbar">
-            <a href="#home">Home</a>
-            <a href="#recipe">Recipe</a>
-            <a href="#about">About</a>
-            <a href="#contact">Contact</a>
-            <div class="dropdown">
-                <button class="dropbtn">
-                    <?php if(isset($_SESSION['username'])){ echo$_SESSION['username'];}?>
-                    <i class="fa fa-caret-down"></i>
-                </button>
-                <div class="dropdown-content">
-                    <a href="logout.php">Logout</a>
+            <a href="#recipe">Home</a>
   
                 </div>
             </div>
@@ -43,18 +33,6 @@
 
     </header>
 
-  <!-- Home Page -->
-
-    <section class="home" id="home">
-
-        <div class="content">
-            <h1>Find A Delicious Meal to Make!</h1>
-            <p>We have recipes and tutorials on how to make each one at home.</p>
-            <a href="service.php"><button class="btn">Click Here!</button></a>
-        </div>
-
-    </section>
- 
   <!-- Recipe Section-->
 
     <section class="recipe" id="recipe">
